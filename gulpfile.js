0a06443586af4d8984edf64f41e9cb92fc8ec276
Nokey <bi.jiankun@cgtn.com>
2017-12-13 12:01:55 +0800

    U: ejs config :wrench:

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * @Author: Nokey 
  * @Date: 2016-11-22 15:30:31 
  * @Last Modified by: Mr.B
- * @Last Modified time: 2017-11-27 17:55:56
+ * @Last Modified time: 2017-12-13 11:59:18
  */
 'use strict';
 
@@ -69,7 +69,7 @@ gulp.task('build', (cb)=>{
  * 启动Gulp，开始监听！:)
  */
 gulp.task('watch', ['dev'], ()=>{
-    gulp.watch(['./rev/**/*.json', './src/*.ejs'], ['ejs']);
+    gulp.watch(['./rev/**/*.json', './src/*.ejs', './src/pages/*.ejs'], ['ejs']);
     gulp.watch('./src/scripts/*.js', ['browserify']);
     gulp.watch('./src/stylus/*.styl', ['stylus']);
     gulp.watch(['./src/scripts/plugins/**/*', './src/media/**/*'], ['static']);
